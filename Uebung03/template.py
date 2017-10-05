de582a94e7a3d00367f71df4e4fa5e181fcd6662
hekrause <hekrause@htwg-konstanz.de>
2017-10-05 16:36:48 +0200

    Uebung03 not finished.

--- Uebung03/template.py
+++ Uebung03/template.py
@@ -2,14 +2,25 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import math
 
 
 def compute_cumHisto(img, binSize=1):
 
-    pass
+    return np.cumsum(bin_Histo(img, binSize))
+
+
+def bin_Histo(img, bin=1):
+    intervalls = np.ceil(256 / bin)
+    histo = np.zeros(shape=intervalls)
+
+    for x in range(0, img.width):
+        for y in range(0, img.height):
+            brightness = round(img.getpixel((x, y)), 0)
+            index = (brightness * intervalls) / 256
+            histo[index] += 1
 
     return histo
-
 
 
 def match_Histo(img_histo, ref_histo):
@@ -17,14 +28,27 @@ def match_Histo(img_histo, ref_histo):
     #img_histo . . . original histogram
     #ref_histo . . . reference histogram
     #returns the mapping function LUT to be applied to the image
-    pass
+
+    LUT = np.zeros(shape=256, dtype=np.int)
+
+    for i in range(0,255):
+        P_i = img_histo[i] / img_histo[255]
+        for j in range(0, 255):
+            P_j = ref_histo[j] / ref_histo[255]
+            if P_i == P_j :
+                LUT[i] = j
+                break;
 
     return LUT
 
 
 def apply_LUT(img, lut):
 
-    pass
+    for x in range(0, img.width):
+        for y in range(0, img.height):
+            edit = img.getpixel((x, y))
+            lut_edit = lut[edit].item()
+            img.putpixel((x, y), lut_edit)
 
     return img
 
@@ -32,32 +56,32 @@ def apply_LUT(img, lut):
 
 def rgb2gray(rgb):
 
-    pass
-
-    return gray
+    # convert to grayscale image (only one channel)
+    return rgb.convert('L')
 
 
 if __name__ == "__main__":
 
     # read img
-    im = Image.open("bild01.jpg")
+    img = Image.open("bild01.jpg")
     ref = Image.open("bild02.jpg")
 
-    # convert to numpy array
-
     # convert to grayscale
+    img = rgb2gray(img)
+    ref = rgb2gray(ref)
 
     # compute histograms
-    # histo_im
-    # histo_ref
+    img_histo = compute_cumHisto(img, 1)
+    ref_histo = compute_cumHisto(ref, 1)
 
     # compute mapping function (LUT) for matching histograms
+    LUT = match_Histo(img_histo, ref_histo)
 
     # compute new image with lut
-    # im_new
+    img_new = apply_LUT(img, LUT)
 
     # compute new histogram of new image
-    # histo_new
+    histo_new = compute_cumHisto(img_new, 1)
 
     # plot information
     N = histo_new.size
@@ -72,17 +96,16 @@ if __name__ == "__main__":
     # plot new img
     plt.figure(1)
     plt.subplot(212)
-    plt.imshow(im_new, cmap = cm.Greys_r)
+    plt.imshow(img_new, cmap = cm.Greys_r)
 
     # plot reference histogram
     plt.figure(2)
     plt.subplot(211)
-    plt.bar(x, histo_ref, width, color="blue")
+    plt.bar(x, ref_histo, width, color="blue")
     plt.xlim([0,N-1])
     # plot reference image
     plt.figure(2)
     plt.subplot(212)
     plt.imshow(ref, cmap = cm.Greys_r)
-
+    
     plt.show()
-
